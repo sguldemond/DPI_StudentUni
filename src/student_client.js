8f4c6541577f2a800cb1a0fb7cc978c651aeef15
stan-is-laus <stanguldemond@gmail.com>
2018-05-31 16:54:32 +0200

    key export-import working

--- src/student_client.js
+++ src/student_client.js
@@ -4,19 +4,40 @@ var amqp = require('amqplib/callback_api');
 var NodeRSA = require('node-rsa');
 var uuid = require('uuid/v4');
 var args = process.argv;
+var fs = require('fs');
+var pemtools = require('pemtools');
 
 var connection;
-var key = new NodeRSA({b:512});
 
+var optionArg = args[2];
 
-amqp.connect('amqp://localhost', function (err, conn) {
-    if(err != undefined) {
-        console.error(err);
-    } else {
-        connection = conn;
-        sendMessage()
-    }
-});
+if(optionArg === 'send') {
+    amqp.connect('amqp://localhost', function (err, conn) {
+        if(err != undefined) {
+            console.error(err);
+        } else {
+            connection = conn;
+            sendMessage()
+        }
+    });
+} else if(optionArg === 'keypair') {
+    var key2 = new NodeRSA({b:512});
+    var privateKey = key2.exportKey('private');
+    var folder = args[3];
+
+    fs.writeFile(folder + "/private_rsa.pem", privateKey, function (err) {
+        if(err) {
+            console.log(err);
+        }
+    });
+
+    var publicKey = key2.exportKey('public');
+    fs.writeFile(folder + "/public_rsa.pem", publicKey, function (err) {
+        if(err) {
+            console.log(err);
+        }
+    });
+}
 
 function sendMessage() {
     connection.createChannel(function(err, ch) {
@@ -30,12 +51,18 @@ function sendMessage() {
             }, {noAck:true});
 
             // var message = 'Hello RPC world!';
-            var message = args[2];
+            var message = args[3];
 
             var studentNo = "254083";
 
+            var privateKey = fs.readFileSync(args[4]);
+
+            var key = new NodeRSA({b:512});
+            key.importKey(privateKey, 'private');
+
             var ecrMessage = key.encryptPrivate(message, 'base64', 'utf-8').toString();
             var publicPem = key.exportKey('public');
+            console.log(publicPem);
 
             var data = {
                 student_no: studentNo,
